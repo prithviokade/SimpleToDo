d1fdf3f4362452d63c3ed8259a463f2f4467f2dc
Prithvi Okade <prithviro@fb.com>
2020-06-24 00:24:15 -0400

    added design of todo list

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
